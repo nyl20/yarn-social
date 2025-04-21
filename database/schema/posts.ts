f514b56e61aed1a3b1f74b000012c85397d251ac
nyl20 <nicole.yliao@gmail.com>
2025-04-21 01:38:20 -0400

    Set up database

---
+++ database/schema/posts.ts
@@ -0,0 +1,49 @@
+import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
+
+import { users } from "./auth"
+import { relations } from "drizzle-orm"
+import { createSelectSchema, createInsertSchema } from "drizzle-zod"
+import { z } from "zod"
+
+
+export const posts = pgTable("posts", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: text("title").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  category: text("type"),
+  description: text("description"),
+  image: text("image"),
+  tag: text("tag")
+})
+
+export const usersRelations = relations(users, ({ many }) => ({
+  profiles: many(posts),
+}))
+
+export const postsRelations = relations(posts, ({ one }) => ({
+  user: one(users, {
+    fields: [posts.userId],
+    references: [users.id],
+  }),
+}))
+
+
+export const selectUserSchema = createSelectSchema(users);
+export type User = z.infer<typeof selectUserSchema>;
+
+export const selectPostSchema = createSelectSchema(posts);
+export type Todo = z.infer<typeof selectPostSchema>;
+
+export const insertPostSchema = createInsertSchema(posts, {
+  title: z.string().nonempty("Title cannot be empty"),
+})
+export type NewPost = z.infer<typeof insertPostSchema>;
+
+export default {
+  users,
+  posts
+};
---
+++ database/schema/profiles.ts
@@ -0,0 +1,50 @@
+import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
+
+import { users } from "./auth"
+import { relations } from "drizzle-orm"
+import { createSelectSchema, createInsertSchema } from "drizzle-zod"
+import { z } from "zod"
+
+
+export const profiles = pgTable("profiles", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  username: text("username").notNull(),
+  completed: boolean("completed").notNull().default(false),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  type: text("type"),
+  bio: text("bio"),
+  image: text("image"),
+  url: text("url")
+})
+
+export const usersRelations = relations(users, ({ many }) => ({
+  profiles: many(profiles),
+}))
+
+export const profilesRelations = relations(profiles, ({ one }) => ({
+  user: one(users, {
+    fields: [profiles.userId],
+    references: [users.id],
+  }),
+}))
+
+
+export const selectUserSchema = createSelectSchema(users);
+export type User = z.infer<typeof selectUserSchema>;
+
+export const selectProfileSchema = createSelectSchema(profiles);
+export type Todo = z.infer<typeof selectProfileSchema>;
+
+export const insertProfileSchema = createInsertSchema(profiles, {
+  username: z.string().nonempty("Username cannot be empty"),
+})
+export type NewProfile = z.infer<typeof insertProfileSchema>;
+
+export default {
+  users,
+  profiles
+};
