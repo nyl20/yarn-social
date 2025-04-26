eda9af514471036a419e5c2da82b2716d74e8781
Wei <xiawei29@hotmail.com>
2025-04-26 11:07:51 -0400

    Debug Verel

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,3 @@
-import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import { db } from '@/database/db'
