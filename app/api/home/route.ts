f8785046eb6c508db83638dabaadd721ac08398d
nyl20 <nicole.yliao@gmail.com>
2025-05-01 20:05:27 -0400

    Debug category selection

--- app/api/home/route.ts
+++ app/api/home/route.ts
@@ -2,13 +2,14 @@ import { NextResponse } from 'next/server'
 // import { getServerSession } from 'next-auth'
 // import { authOptions } from '@/lib/auth'
 import { db } from '@/database/db'
-// import { posts } from '@/database/schema/posts'
+import { posts } from '@/database/schema/posts'
+import { eq } from 'drizzle-orm'
 
 
 export async function GET() {
   try {
     const patternsData = await db.query.posts.findMany({
-      where: (posts, { eq }) => eq(posts.category, 'pattern'),
+      where: eq(posts.category, 'Pattern'),
       limit: 5,
       with: {
         user: {
@@ -21,7 +22,7 @@ export async function GET() {
       orderBy: (posts, { desc }) => [desc(posts.views)]
     })
     const shopsData = await db.query.posts.findMany({
-      where: (posts, { eq }) => eq(posts.category, 'shop'),
+      where: eq(posts.category, 'Shop'),
       limit: 5,
       with: {
         user: {
